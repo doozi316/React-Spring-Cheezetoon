6ae0cf8d339aae2cb849d57219b5892764cb1139
doozi316 <doozi316@gmail.com>
2019-11-08 22:30:51 +0900

    테스트 java 삭제 및 JAVA 8 날짜 / 시간 변환기, UTC 사용 설정

--- src/main/java/com/example/cheezetoon/DemoApplication.java
+++ src/main/java/com/example/cheezetoon/DemoApplication.java
@@ -1,11 +1,23 @@
 package com.example.cheezetoon;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+//자동 convert
+@EntityScan(basePackageClasses = {DemoApplication.class, Jsr310JpaConverters.class})
 public class DemoApplication {
 
+	@PostConstruct
+	void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/cheezetoon/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cheezetoon;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
