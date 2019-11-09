0bb8ec958428ccf484fd582b90205f37f1ec2042
doozi316 <doozi316@gmail.com>
2019-11-09 15:13:01 +0900

    미인증 접근 시 예외처리용 JwtAuthenticationEntryPoint 구현

---
+++ src/main/java/com/example/cheezetoon/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.example.cheezetoon.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//AuthenticationEntryPoint는 commence()-미인증 사용자 접근 시 예외 처리 제공
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    }
+}
