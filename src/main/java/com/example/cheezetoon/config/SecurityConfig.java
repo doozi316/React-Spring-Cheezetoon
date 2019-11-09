84261669ee40a7c4fb1dc68e71bf04d6bac0438d
doozi316 <doozi316@gmail.com>
2019-11-09 15:39:47 +0900

    UserPrincipal, 즉, UserDetail 구현

--- src/main/java/com/example/cheezetoon/config/SecurityConfig.java
+++ src/main/java/com/example/cheezetoon/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.cheezetoon.config;
 
+import com.example.cheezetoon.security.JwtAuthenticationEntryPoint;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
