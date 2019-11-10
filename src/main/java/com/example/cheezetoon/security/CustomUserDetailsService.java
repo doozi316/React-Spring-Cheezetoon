c9e67997c6d99c9cfc21b8333c0894599406dc21
doozi316 <doozi316@gmail.com>
2019-11-10 15:34:59 +0900

    CustomUserDetailsService

---
+++ src/main/java/com/example/cheezetoon/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.example.cheezetoon.security;
+
+import com.example.cheezetoon.model.User;
+import com.example.cheezetoon.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service //Repository를 통해 db에서 데이터를 가져온 후 컨트롤러에게 전달해주는 클래스
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional //트랜잭션-정상 여부에 따라 Commit, Rollback
+    public UserDetails loadUserByUsername(String usernameOrEmail)
+            throws UsernameNotFoundException {
+        // username 또는 email로 로그인 하게 함
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> 
+                        new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    // This method is used by JWTAuthenticationFilter
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
