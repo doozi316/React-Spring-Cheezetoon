9ce6e8e82b6e1752c0adc376730337afb2447b1c
doozi316 <doozi316@gmail.com>
2019-11-13 01:44:32 +0900

    라우터 구현 및 app header 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,124 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  Route,
+  withRouter,
+  Switch
+} from 'react-router-dom';
+
+import { getCurrentUser } from './util/APIUtils';
+import { ACCESS_TOKEN } from './constants';
+
 import './App.css';
 import Main from "./container/Main";
 import WebtoonHome from "./container/WebtoonHome";
 import Viewer from "./container/Viewer";
+
 import Login from "./user/login/Login";
-import SignUp from "./user/signup/Signup";
+import Signup from "./user/signup/Signup";
+import Profile from './user/profile/Profile';
+import AppHeader from './common/AppHeader';
+import NotFound from './common/NotFound';
+import LoadingIndicator from './common/LoadingIndicator';
+
+
+import { Layout, notification } from 'antd';
+const { Content } = Layout;
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          currentUser: null,
+          isAuthenticated: false,
+          isLoading: false
+        }
+        this.handleLogout = this.handleLogout.bind(this);
+        this.loadCurrentUser = this.loadCurrentUser.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+
+        notification.config({
+            placement: 'topRight',
+            top: 70,
+            duration: 3,
+        });    
+    }
+
+    loadCurrentUser() {
+        this.setState({
+          isLoading: true
+        });
+        getCurrentUser()
+        .then(response => {
+          this.setState({
+            currentUser: response,
+            isAuthenticated: true,
+            isLoading: false
+          });
+        }).catch(error => {
+          this.setState({
+            isLoading: false
+          });  
+        });
+      }
+
+      componentDidMount() {
+        this.loadCurrentUser();
+      }
+    
+      handleLogout(redirectTo="/", notificationType="success", description="로그아웃 되었습니다.") {
+        localStorage.removeItem(ACCESS_TOKEN);
+    
+        this.setState({
+          currentUser: null,
+          isAuthenticated: false
+        });
+    
+        this.props.history.push(redirectTo);
+        
+        notification[notificationType]({
+          message: 'Cheeze Toon',
+          description: description,
+        });
+      }
+
+      handleLogin() {
+        notification.success({
+          message: 'Cheeze Toon',
+          description: "로그인 되었습니다.",
+        });
+        this.loadCurrentUser();
+        this.props.history.push("/");
+      }
+
     render() {
+        if(this.state.isLoading) {
+            return <LoadingIndicator />
+          }
         return (
-            <Router>
-                <div>
-                    <Route exact path="/" component={Main} />
-                    {/* ":" 뒤에 있는 것은 prams */}
-                    <Route path="/webtoon/:webtoonId" component={WebtoonHome} /> 
-                    <Route path="/viewer/:episodeId" component={Viewer} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/signup" component={SignUp} />
+            <Layout className="app-container">
+                <AppHeader isAuthenticated={this.state.isAuthenticated} 
+                    currentUser={this.state.currentUser} 
+                    onLogout={this.handleLogout} />
+
+            <Content className="app-content">
+                <div className="container">
+                  <Switch>
+                        <Route exact path="/" component={Main} />
+                        {/* ":" 뒤에 있는 것은 prams */}
+                        <Route path="/webtoon/:webtoonId" component={WebtoonHome} /> 
+                        <Route path="/viewer/:episodeId" component={Viewer} />
+                        <Route path="/login" 
+                        render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
+                        <Route path="/signup" component={Signup}></Route>
+                        <Route path="/users/:username" 
+                        render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
+                        </Route>
+                        <Route component={NotFound}></Route>
+                  </Switch>
                 </div>
-            </Router>);
+            </Content>
+        </Layout>);
     }
 }
-export default App;
+export default withRouter(App);
 
--- frontend/src/container/Main.js
+++ frontend/src/container/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Header from "../component/Header";
-import Gnb from "../component/Gnb";
-import Footer from "../component/Footer";
+// import Header from "../component/Header";
+// import Gnb from "../component/Gnb";
+// import Footer from "../component/Footer";
 import WebtoonList from "../component/WebtoonList";
 
 class Main extends Component{
@@ -65,8 +65,8 @@ class Main extends Component{
         
         return (
             <div>
-                <Header />
-                <Gnb day={this.state.day} />
+                {/* <Header />
+                <Gnb day={this.state.day} /> */}
                 <main>
                 { this.state.webtoonList.length > 0 ? (
                     <WebtoonList 
@@ -77,7 +77,7 @@ class Main extends Component{
                     </span>
                 )}
                 </main>
-                <Footer />
+                {/* <Footer /> */}
             </div>
         )
     }
