d0616b670a16c434a61f05a82f61376fe11c58f0
doozi316 <doozi316@gmail.com>
2019-11-13 15:01:57 +0900

    사이드 메뉴 구성

--- frontend/src/common/AppHeader.js
+++ frontend/src/common/AppHeader.js
@@ -2,8 +2,43 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import './AppHeader.css';
 import { Layout, Menu, Dropdown, Icon } from 'antd';
-import { SubMenu } from 'rc-menu';
-const Header = Layout.Header;
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+
+function ProfileDropdownMenu(props) {
+  const dropdownMenu = (
+    <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
+      <Menu.Item key="user-info" className="dropdown-item" disabled>
+        <div className="user-full-name-info">
+          {props.currentUser.name}
+        </div>
+        <div className="username-info">
+          @{props.currentUser.username}
+        </div>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="profile" className="dropdown-item">
+        <Link to={`/users/${props.currentUser.username}`}>Profile</Link>
+      </Menu.Item>
+      <Menu.Item key="logout" className="dropdown-item">
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+      <Dropdown 
+        overlay={dropdownMenu} 
+        trigger={['click']}
+        getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
+        <a className="ant-dropdown-link">
+           <Icon type="user" className="nav-icon" style={{marginRight: 0}} /> <Icon type="down" />
+        </a>
+      </Dropdown>
+    );
+  }
 
 class AppHeader extends Component {
     constructor(props) {
@@ -18,19 +53,6 @@ class AppHeader extends Component {
     }
 
     render() {
-        let menuItem;
-
-        menuItem=[
-          <SubMenu title="요일별">
-            <Menu.Item key="/">
-              <Link to ="/">월</Link>
-            </Menu.Item>
-            <Menu.Item key="/">
-              <Link to ="/">화</Link>
-            </Menu.Item>
-          </SubMenu>
-        ]
-
         let menuItems;
         if(this.props.currentUser) {
           menuItems = [
@@ -42,15 +64,17 @@ class AppHeader extends Component {
         ]; 
     } else {
         menuItems = [
-        <Menu.Item key="/login" className="left1">
+        <Menu.Item key="/login">
             <Link to="/login">Login</Link>
         </Menu.Item>,
-        <Menu.Item key="/signup" className="left2">
+        <Menu.Item key="/signup">
             <Link to="/signup">Signup</Link>
         </Menu.Item>                  
         ];
     }
+
     return (
+      <Layout>
         <Header className="app-header">
           <div className="container">
             <div className="app-title" >
@@ -61,47 +85,68 @@ class AppHeader extends Component {
                 mode="horizontal"
                 selectedKeys={[this.props.location.pathname]}
                 style={{ lineHeight: '64px' }} >
-                  {menuItem}
                   {menuItems}
               </Menu>
           </div>
           </Header>
+          <Layout>
+            <Sider width={200} style={{ background: '#FFC726' }} className="app-sider">
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+              >
+                <SubMenu
+                  key="sub1"
+                  title={
+                    <span>
+                      요일별
+                    </span>
+                  }
+                >
+                  <Menu.Item key="1">
+                    <Link to="/?day=mon">월요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Link to="/?day=tue">화요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Link to="/?day=wed">수요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Link to="/?day=thu">목요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="5">
+                    <Link to="/?day=fri">금요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="6">
+                    <Link to="/?day=sat">토요일</Link>
+                  </Menu.Item>
+                  <Menu.Item key="7">
+                    <Link to="/?day=sun">일요일</Link>
+                  </Menu.Item>
+                </SubMenu>
+                <SubMenu
+                  key="sub2"
+                  title={
+                    <span>
+                      장르별
+                    </span>
+                  }
+                >
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+          </Layout>
+      </Layout>
         );
     }
 }
-
-function ProfileDropdownMenu(props) {
-    const dropdownMenu = (
-      <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
-        <Menu.Item key="user-info" className="dropdown-item" disabled>
-          <div className="user-full-name-info">
-            {props.currentUser.name}
-          </div>
-          <div className="username-info">
-            @{props.currentUser.username}
-          </div>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="profile" className="dropdown-item">
-          <Link to={`/users/${props.currentUser.username}`}>Profile</Link>
-        </Menu.Item>
-        <Menu.Item key="logout" className="dropdown-item">
-          Logout
-        </Menu.Item>
-      </Menu>
-    );
-  
-    return (
-        <Dropdown 
-          overlay={dropdownMenu} 
-          trigger={['click']}
-          getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
-          <a className="ant-dropdown-link">
-             <Icon type="user" className="nav-icon" style={{marginRight: 0}} /> <Icon type="down" />
-          </a>
-        </Dropdown>
-      );
-    }
     
     
     export default withRouter(AppHeader);
